47d1b126e0dfe93036e547ddbfe162bff2dfac31
Deus Ex Machina <quiocdat777@gmail.com>
2024-07-24 09:29:19 +0700

    apply changes

--- src/test/java/com/java/webdevelopment/shopping_app/services/CategoryServiceTest.java
+++ src/test/java/com/java/webdevelopment/shopping_app/services/CategoryServiceTest.java
@@ -1,10 +1,6 @@
 package com.java.webdevelopment.shopping_app.services;
 
-
-
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -29,40 +25,6 @@ public class CategoryServiceTest {
     }
 
     @Test
-    void testDeleteCategory() {
->>>>>>> a76dcbc7950cf60a43cd638ea9b1bf0a19459976:src/test/java/com/java/webdevelopment/shopping_app/services/CategoryServiceTest.java
-
-    Faker faker;
-
-    UserPrincipal userPrincipal;
-
-    @BeforeEach
-    void prepare() {
-        faker = new Faker();
-        Product product = Product.builder()
-        .id("MlSpZnV0nhyfrW-_OWj5w")
-        .name("smartphone")
-        .price(90000l)
-        .category(null)
-        .description("smartphone")
-        .images(null).build();
-        productRepository.save(product);   
-    }
-
-    @Test
-<<<<<<< HEAD:src/test/java/com/java/webdevelopment/shopping_app/services/ProductServiceTest.java
-    void testCreateProduct() {
-        ProductDTO productDTO = new ProductDTO();
-        productDTO.setName("smartphone");
-        productDTO.setPrice(900000l);
-        productDTO.setAmount(100);
-        productDTO.setCategory(new Category());
-        productDTO.setDescription("sanpham");
-        productDTO.setImages(null);
-        ProductDTO res = productService.createProduct(productDTO);
-        System.out.println(res);
-    }
-=======
     void testGetAllCategory() {
 
     }
@@ -75,7 +37,6 @@ public class CategoryServiceTest {
     @Test
     void testUpdateCategory() {
 
->>>>>>> a76dcbc7950cf60a43cd638ea9b1bf0a19459976:src/test/java/com/java/webdevelopment/shopping_app/services/CategoryServiceTest.java
     }
 
 }
